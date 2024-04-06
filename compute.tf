0ebd3d91539af59b5ddab55e3d87da4f9c00056d
Ravish Tiwari <ravisht76@gmail.com>
2024-04-06 23:38:17 +0530

    fixed code

--- compute.tf
+++ compute.tf
@@ -4,10 +4,6 @@ resource "oci_core_boot_volume" "rtlinux_boot_volume" {
   availability_domain = var.availability_domain
   display_name        = "rtlinux-boot-volume"
   size_in_gbs         = 50  # Adjust size as needed
-
-  source_details {
-    source_type = "none"
-  }
 }
 
 # Block Volume
