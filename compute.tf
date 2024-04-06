aec8e41be7f7ce913107459e7e22bd349803660a
Ravish Tiwari <ravisht76@gmail.com>
2024-04-06 23:32:48 +0530

    fixed code

--- compute.tf
+++ compute.tf
@@ -1,16 +1,19 @@
 resource "oci_core_boot_volume" "rtlinux_boot_volume" {
-  compartment_id = var.compartment_id
+  compartment_id      = var.compartment_id
   availability_domain = var.availability_domain
-  display_name   = "rtlinux-boot-volume"
-  size_in_gbs    = 50  # Adjust size as needed
-  
+  display_name        = "rtlinux-boot-volume"
+  size_in_gbs         = 50  # Adjust size as needed
+
+  source_details {
+    source_type = "none"
+  }
 }
 
 resource "oci_core_volume" "rtlinux_block_volume" {
-  compartment_id = var.compartment_id
+  compartment_id      = var.compartment_id
   availability_domain = var.availability_domain
-  display_name   = "rtlinux-block-volume"
-  size_in_gbs    = 100  # Adjust size as needed
+  display_name        = "rtlinux-block-volume"
+  size_in_gbs         = 100  # Adjust size as needed
 }
 
 resource "oci_core_instance" "rtlinux_instance" {
