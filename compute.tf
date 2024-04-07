fa55adc664f1f2b73330c742e35b539535d40a15
Ravish Tiwari <ravisht76@gmail.com>
2024-04-07 16:44:39 +0530

    fixed wrong variable

--- compute.tf
+++ compute.tf
@@ -1,17 +1,3 @@
-# Boot Volume
-resource "oci_core_boot_volume" "rtlinux_boot_volume" {
-  compartment_id      = var.compartment_id
-  availability_domain = var.availability_domain
-  display_name        = "rtlinux-boot-volume"
-}
-
-# Block Volume
-resource "oci_core_volume" "rtlinux_block_volume" {
-  compartment_id      = var.compartment_id
-  availability_domain = var.availability_domain
-  display_name        = "rtlinux-block-volume"
-  size_in_gbs         = 100  # Adjust size as needed
-}
 
 resource "oci_core_instance" "rtlinux_instance" {
   availability_domain = var.availability_domain
@@ -36,7 +22,7 @@ resource "oci_core_instance" "rtlinux_instance" {
 
    source_details {
     source_type = "image"
-    source_id = var.instance_image
+    source_id = var.linuxvm_image_id
     boot_volume_size_in_gbs = var.boot_volume_size_in_gbs
   } 
   
