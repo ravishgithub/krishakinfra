5e0c4302b013f87549409c09a7483a71808084f3
Ravish Tiwari <ravisht76@gmail.com>
2024-04-07 21:49:09 +0530

    fix network association & security

--- networksecurity.tf
+++ networksecurity.tf
@@ -10,32 +10,32 @@ resource "oci_core_security_list" "public_security_list" {
   }
 
   ingress_security_rules {
-    protocol    = "6"   # TCP protocol for SSH and HTTP
-    source      = "${oci_core_security_list.private_security_list.id}"
+    protocol    = "6"   # TCP protocol for SSH
+    source      = "${oci_core_security_list.private_security_list.id}"  # Replace with appropriate source
     tcp_options {
       destination_port_range = "22"   # SSH port
     }
   }
 
   ingress_security_rules {
-    protocol    = "6"   # TCP protocol for SSH and HTTP
-    source      = "${oci_core_security_list.private_security_list.id}"
+    protocol    = "6"   # TCP protocol for RDP
+    source      = "${oci_core_security_list.private_security_list.id}"  # Replace with appropriate source
     tcp_options {
       destination_port_range = "3389"   # RDP port
     }
   }
 
   ingress_security_rules {
-    protocol    = "6"   # TCP protocol for SSH and HTTP
-    source      = "0.0.0.0/0"  # Allow from anywhere for HTTP/HTTPS
+    protocol    = "6"   # TCP protocol for HTTP
+    source      = "0.0.0.0/0"  # Allow from anywhere for HTTP
     tcp_options {
       destination_port_range = "80"   # HTTP port
     }
   }
 
   ingress_security_rules {
-    protocol    = "6"   # TCP protocol for SSH and HTTP
-    source      = "0.0.0.0/0"  # Allow from anywhere for HTTP/HTTPS
+    protocol    = "6"   # TCP protocol for HTTPS
+    source      = "0.0.0.0/0"  # Allow from anywhere for HTTPS
     tcp_options {
       destination_port_range = "443"   # HTTPS port
     }
@@ -47,7 +47,19 @@ resource "oci_core_network_security_group" "private_network_security_group" {
   compartment_id = var.compartment_id
   vcn_id         = oci_core_virtual_network.kheti_vcn.id
   display_name   = "private-network-security-group"
-  
+
+  egress_security_rules {
+    destination     = "0.0.0.0/0"
+    protocol        = "all"
+  }
+
+  ingress_security_rules {
+    protocol    = "6"   # TCP protocol for ingress traffic
+    source      = "${oci_core_security_list.public_security_list.id}"
+    tcp_options {
+      destination_port_range = "0-65535"   # Allow all ports from the public security list
+    }
+  }
 }
 
 # Create a security list for the private subnet
@@ -62,7 +74,7 @@ resource "oci_core_security_list" "private_security_list" {
   }
 
   ingress_security_rules {
-    protocol    = "all"   # Allow all protocols
-    source      = oci_core_network_security_group.private_network_security_group.id
+    protocol    = "6"   # TCP protocol for ingress traffic
+    source      = "${oci_core_network_security_group.private_network_security_group.id}"
   }
 }
