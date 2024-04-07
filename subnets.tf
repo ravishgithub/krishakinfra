95a86b84d8618a84ce9e3d1cfd2c291e049da125
Ravish Tiwari <ravisht76@gmail.com>
2024-04-07 22:12:17 +0530

    gateway & rout table associations

--- subnets.tf
+++ subnets.tf
@@ -19,9 +19,23 @@ resource "oci_core_subnet" "kheti_private_subnet" {
 resource "oci_core_route_table" "kheti_public_route" {
   vcn_id = oci_core_virtual_network.kheti_vcn.id
   compartment_id = var.compartment_id
+
+  # Route rule for internet-bound traffic
+  route {
+    destination     = "0.0.0.0/0"
+    destination_type = "CIDR_BLOCK"
+    network_entity_id = oci_core_internet_gateway.kheti_igw.id
+  }
 }
 
 resource "oci_core_route_table" "kheti_private_route" {
   vcn_id = oci_core_virtual_network.kheti_vcn.id
   compartment_id = var.compartment_id
+
+   # Route rule for NAT gateway for internet access
+  route {
+    destination     = "0.0.0.0/0"
+    destination_type = "CIDR_BLOCK"
+    network_entity_id = oci_core_nat_gateway.kheti_nat_gw.id
+  }
 }
