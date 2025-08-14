5810b5bda3011e0ee490b6a09309c83bd0a692bd
Ravish Tiwari <ravisht76@gmail.com>
2025-08-14 21:26:37 +0530

    Fix ORM stack: create compartment within root

--- compartment.tf
+++ compartment.tf
@@ -1,4 +1,6 @@
+# compartment.tf
 resource "oci_identity_compartment" "krishak_compartment" {
+  compartment_id = var.tenancy_ocid   # <-- parent is the TENANCY (root)
   name           = "krishakcompartment"
   description    = "Compartment for Krishak application resources"
   enable_delete  = true
