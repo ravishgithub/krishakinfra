1503cb2d63210a5a6501f8f021f33cc78185a70e
Ravish Tiwari <ravisht76@gmail.com>
2025-08-16 22:13:49 +0530

    Fix: de-duplicate outputs (keep in outputs.tf only)

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,19 @@
+# outputs.tf
+
+# Compartment OCID (created by this stack)
 output "compartment_id" {
   value       = oci_identity_compartment.krishak_compartment.id
   description = "OCID of the Krishak compartment"
 }
 
+# Identity Domain OCID (null if not created)
 output "identity_domain_id" {
-  value       = oci_identity_domain.krishak_domain.id
-  description = "OCID of the created Identity Domain"
+  value       = var.create_identity_domain && length(oci_identity_domain.krishak_domain) > 0 ? oci_identity_domain.krishak_domain[0].id : null
+  description = "OCID of the Identity Domain (null if create_identity_domain=false)"
 }
 
+# Public URL for your frontend index.html
 output "frontend_bucket_url" {
   value       = "https://objectstorage.${var.region}.oraclecloud.com/n/${data.oci_objectstorage_namespace.ns.namespace}/b/${oci_objectstorage_bucket.krishak_frontend_bucket.name}/o/index.html"
-  description = "URL to access the frontend hosted in Object Storage"
+  description = "Public URL to index.html in the frontend bucket"
 }
